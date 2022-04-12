430a74052dd8ef87145fbc5462811eccbd3b4afc
mikegeldart <GLDMIC023@myuct.ac.za>
2022-04-12 20:22:23 +0200

    2nd commit

--- tidy code.R
+++ tidy code.R
@@ -1,58 +1,61 @@
+
+setwd("C:/Users/micge/Desktop/project/data")
+
+getwd()
+
 library(tidyverse)
 require(tidyverse)
 
 library(readxl)
 require(readxl)
 
-setwd("C:/Users/micge/Desktop/project/data")
-
-getwd()
 
 excel_sheets("abalone data.xlsx")
 
 
 abdat<-read_xlsx("abalone data.xlsx", sheet= "All Trial pH & Temp ")
+
 head(abdat)  
+
 attach(abdat)
-detach(abdat)
+
 abdat
+
 names(abdat)
 
-
-abdat %>% 
-filter(Circulation == "Raceway")
-select() 
- 
-attach(abdat)         
-detach(abdat) 
-
-range("Abalone 2.1 (Prob 1) - Temperature   (°C)")
 summary(abdat)
  
 
-abdat<-read_xlsx("abalone data.xlsx", sheet= "All Trial pH & Temp ", range = "B1:I2711")
+abdat<-read_xlsx("abalone data.xlsx", sheet= "All Trial pH & Temp ", range = "A1:I1711")
 
-  pivot_longer(cols = c("Abalone2.1(Prob 1)-Temperature(°C)",
+
+
+pivot_longer(cols = c("Abalone2.1(Prob 1)-Temperature(°C)",
                         "Abalone2.1(Prob 1)-pH",             
                         "Abalone2.2(Prob 2)-Temperature(°C)",
                         "Abalone2.2(Prob 2)-pH",             
                         "Abalone3.1(Prob 3)-Temperature(°C)",
-                        "Abalone3.1(Prob 3)-pH" ), names_to = "variable", values_to = "value") 
+                        "Abalone3.1(Prob 3)-pH" ) 
  
-class(abdat)
+
 abdat
+
 unclass(Circulation)
-abdat$Circulation=as.double(abdat$Circulation)
+
+attach(abdat)
+detach(abdat)
+
+abdat$Circulation=as.character(abdat$Circulation)
 unclass(Circulation)
+
 summary(abdat)
-group_by(Circulation)
-abdat
+
+
 
 library(ggplot2)
 require(ggplot2)
 
-ggplot() +
-    geom_boxplot(aes(y = value, x = Site)) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    facet_wrap(vars(variable), scales = "free")
-     
+names(abdat)
+
+plot(temporal, Aba)
+length(abalone)
